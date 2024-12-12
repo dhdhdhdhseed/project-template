db92fa34cb2304d4e60623d92a8dc0dad3643c12
梁承龙 <1807760488@qq.com>
2024-12-12 09:57:49 +0800

    提交

--- src/router/index.ts
+++ src/router/index.ts
@@ -83,45 +83,60 @@ export const constantRoutes: RouteRecordRaw[] = [
       },
     ]
   },
-  
-  // 功能demo
-  // {
-  //   path: "/hook-demo",
-  //   component: Layouts,
-  //   redirect: "/hook-demo/use-fetch-select",
-  //   name: "HookDemo",
-  //   meta: {
-  //     title: "Hook",
-  //     elIcon: "Menu",
-  //     alwaysShow: true
-  //   },
-  //   children: [
-  //     {
-  //       path: "use-fetch-select",
-  //       component: () => import("@/views/hook-demo/use-fetch-select.vue"),
-  //       name: "UseFetchSelect",
-  //       meta: {
-  //         title: "useFetchSelect"
-  //       }
-  //     },
-  //     {
-  //       path: "use-fullscreen-loading",
-  //       component: () => import("@/views/hook-demo/use-fullscreen-loading.vue"),
-  //       name: "UseFullscreenLoading",
-  //       meta: {
-  //         title: "useFullscreenLoading"
-  //       }
-  //     },
-  //     {
-  //       path: "use-watermark",
-  //       component: () => import("@/views/hook-demo/use-watermark.vue"),
-  //       name: "UseWatermark",
-  //       meta: {
-  //         title: "useWatermark"
-  //       }
-  //     }
-  //   ]
-  // }
+  {
+    path: "/hook-demo",
+    component: Layouts,
+    redirect: "/hook-demo/use-fetch-select",
+    name: "HookDemo",
+    meta: {
+      title: "Hook",
+      elIcon: "Menu",
+      alwaysShow: true
+    },
+    children: [
+      {
+        path: "use-fetch-select",
+        component: () => import("@/views/hook-demo/use-fetch-select.vue"),
+        name: "UseFetchSelect",
+        meta: {
+          title: "useFetchSelect"
+        }
+      },
+      {
+        path: "use-fullscreen-loading",
+        component: () => import("@/views/hook-demo/use-fullscreen-loading.vue"),
+        name: "UseFullscreenLoading",
+        meta: {
+          title: "全屏加载hook"
+        }
+      },
+      {
+        path: "use-watermark",
+        component: () => import("@/views/hook-demo/use-watermark.vue"),
+        name: "UseWatermark",
+        meta: {
+          title: "useWatermark"
+        }
+      }
+    ]
+  },
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/test",
+    children: [
+      {
+        path: "test",
+        component: () => import("@/views/test/index.vue"),
+        name: "Test",
+        meta: {
+          title: "测试页面",
+          svgIcon: "dashboard",
+          affix: true
+        }
+      }
+    ]
+  },
 ]
 
 /**
@@ -130,7 +145,7 @@ export const constantRoutes: RouteRecordRaw[] = [
  * 必须带有 Name 属性
  */
 export const dynamicRoutes: RouteRecordRaw[] = [
- 
+
 ]
 
 const router = createRouter({
---
+++ src/views/test/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="test">
+        <h3>测试页面</h3>
+        <Demo></Demo>
+    </div>
+</template>
+
+<script setup>
+import Demo from '@/components/Demo.vue'
+</script>
+
+<style lang="scss">
+.test {}
+</style>
